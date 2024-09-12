8a2f26922c530a4a151e0638b0837bbda9716010
rolandojduartem <rolando@vauxoo.com>
2024-09-12 16:21:02 -0600

    [ADD] auth_session_timeout_bus: add module to fix auth_session_timeout

---
+++ auth_session_timeout_bus/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "Instance Creator",
+    "summary": """
+    Instance creator for auth-session-timeout-bus. This is the app.
+    """,
+    "author": "Vauxoo",
+    "website": "https://www.vauxoo.com",
+    "license": "AGPL-3",
+    "category": "Installer",
+    "version": "15.0.1.0.0",
+    "depends": [
+        "l10n_mx_edi",
+    ],
+    "data": [
+        "data/res_company_data.xml",
+        "data/res_partner_data.xml",
+    ],
+    "demo": [
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": True,
+}
